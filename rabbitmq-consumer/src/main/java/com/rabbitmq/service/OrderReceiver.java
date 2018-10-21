f1d1bd6ca5c05f5359f9092ef898151471d9ffb1
谌剑 <360959136@qq.com>
2018-10-21 17:24:00 +0800

    addConfirmListener

--- rabbitmq-consumer/src/main/java/com/rabbitmq/service/OrderReceiver.java
+++ rabbitmq-consumer/src/main/java/com/rabbitmq/service/OrderReceiver.java
@@ -1,7 +1,10 @@
 package com.rabbitmq.service;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -14,6 +17,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
 import com.rabbitmq.model.OrderPO;
 
 /**
@@ -37,7 +41,32 @@ public class OrderReceiver {
 	       value = @Queue(value = "order-queue", durable = "true"),
 	       key="order.*"
 		))
+	
 	public void getMessageId(@Payload OrderPO orderPO, Channel channel, @Headers Map<String, Object> heads) {
+		
+//		try {
+//		 final	SortedSet<Long> synchronizedSortedSet = Collections.synchronizedSortedSet(new TreeSet<Long>());
+//			channel.confirmSelect();
+//			channel.addConfirmListener(new ConfirmListener() {
+//				//成功回调
+//				@Override
+//				public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+//					// TODO Auto-generated method stub
+//					
+//				}
+//				//失败回调
+//				@Override
+//				public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+//					// TODO Auto-generated method stub
+//					
+//				}
+//			});
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		
+		
 		System.out.println("开始消费消息了！！！！！！！！！！！！！！！");
 		System.out.println("消息ID是：" + orderPO.getMessageId());
 		Long tag = (Long) heads.get(AmqpHeaders.DELIVERY_TAG);
